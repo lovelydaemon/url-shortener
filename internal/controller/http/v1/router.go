8f00c1120f1295c3137936a9eea3d7f84fcc9bf0
lovelydaemon <slamy777@gmail.com>
2024-03-27 23:42:09 +0300

    feat: refactor router, make chi default router, update tests

--- internal/controller/http/v1/router.go
+++ internal/controller/http/v1/router.go
@@ -1,11 +1,19 @@
 package v1
 
 import (
-	"github.com/gin-gonic/gin"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/lovelydaemon/url-shortener/internal/usecase"
 )
 
-// NewRouter -
-func NewRouter(handler *gin.Engine, u usecase.ShortURL) {
-	newShortURLRoutes(handler, u)
+func NewRouter(u usecase.ShortURL) *chi.Mux {
+	r := chi.NewRouter()
+
+	// Middlewares
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	// shortURL get and create
+	r.Mount("/", newShortURLRoutes(u))
+	return r
 }
